bf545403279ed66508bd4cbd27ab090098034600
Lautaro Jayat <lautaro.jayat@gmail.com>
2020-02-03 19:17:50 -0300

    Updates

---
+++ precache-manifest.70ac74d14004b2c6a245b05b9690cf5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ec202987d17b15322aeeed0cfd53ca",
+    "url": "/react-app-example/index.html"
+  },
+  {
+    "revision": "d8976d201996fcbec94f",
+    "url": "/react-app-example/static/css/main.8f18a548.chunk.css"
+  },
+  {
+    "revision": "197278537ac4ea40b18f",
+    "url": "/react-app-example/static/js/2.aa840cf3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-app-example/static/js/2.aa840cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8976d201996fcbec94f",
+    "url": "/react-app-example/static/js/main.60bb3eaa.chunk.js"
+  },
+  {
+    "revision": "9e03e68a22a5feee8e10",
+    "url": "/react-app-example/static/js/runtime-main.6f35fb08.js"
+  }
+]);
